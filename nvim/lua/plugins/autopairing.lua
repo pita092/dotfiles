c6114f0daf88303c64bd920bc55ad593a1e23776
pita <154020297+pita092@users.noreply.github.com>
2024-06-22 19:43:06 +0300

    Delete nvim directory

--- nvim/lua/plugins/autopairing.lua
+++
@@ -1,9 +0,0 @@
-return{
-  'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    config = true,
-     lazy = true,
-    	config = function ()
-		require("nvim-autopairs").setup{}
-    	end
-    }
